a1ea36e5f8dc8653707c6e12bea8806d17fe9d51
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-08-03 00:48:17 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,31 @@
 import EventCard from '../components/Event/EventCard';
 import PageLayout from '../components/PageLayout/PageLayout';
 
+import { useTranslation } from 'react-i18next';
+
 function MainPage() {
+  const { t } = useTranslation();
   return (
-    <PageLayout title="My events">
+    <PageLayout title={t('main-title')}>
       <EventCard />
     </PageLayout>
   );
 }
 
 export default MainPage;
+
+// import EventCard from '../components/Event/EventCard';
+// import PageLayout from '../components/PageLayout/PageLayout';
+
+// import { useI18n } from '../i18nContext';
+
+// function MainPage() {
+//   const { t, i18n } = useI18n();
+//   return (
+//     <PageLayout title={t('title')}>
+//       <EventCard />
+//     </PageLayout>
+//   );
+// }
+
+// export default MainPage;
