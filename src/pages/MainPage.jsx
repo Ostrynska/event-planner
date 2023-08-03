c31ef5aaaab085747faf903aa568103217541138
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-08-03 14:06:54 +0300

    fix

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -14,19 +14,3 @@ function MainPage() {
 }
 
 export default MainPage;
-
-// import EventCard from '../components/Event/EventCard';
-// import PageLayout from '../components/PageLayout/PageLayout';
-
-// import { useI18n } from '../i18nContext';
-
-// function MainPage() {
-//   const { t, i18n } = useI18n();
-//   return (
-//     <PageLayout title={t('title')}>
-//       <EventCard />
-//     </PageLayout>
-//   );
-// }
-
-// export default MainPage;
